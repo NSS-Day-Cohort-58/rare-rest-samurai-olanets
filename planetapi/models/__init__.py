a71462ae9ffa36ea18628273026df75591e24bdc
Mckenzie Rondeau <mckenzierondeau@gmail.com>
2022-11-17 14:23:00 -0600

    added comment fixture and some sample data for db to insert in table with migrations

--- planetapi/models/__init__.py
+++ planetapi/models/__init__.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 from .comment import Comment
-=======
 from .category import Category
-<<<<<<< HEAD
-from .user import User
->>>>>>> main
-=======
 from .author import Author
->>>>>>> 6baa25f4c68a7a736dea73b3f81671f00f74843e
+
--- planetapi/views/__init__.py
+++ planetapi/views/__init__.py
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 from .comment_view import CommentView
-=======
-from .user_view import UserView
+
 from .category_view import CategoryView
->>>>>>> main
-=======
-from .category_view import CategoryView
+
+
 from .author_view import AuthorView
->>>>>>> 6baa25f4c68a7a736dea73b3f81671f00f74843e
+
