b42345232ca543f635d5a9f1fe1ad570cf62caf6
Ameena Mashaikh <ameenamashaikh@gmail.com>
2022-11-18 11:27:15 -0500

    fixed it up

--- planetapi/models/user.py
+++ planetapi/models/user.py
@@ -5,6 +5,6 @@ class User(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=50)
-    profile_img_url = models.URLField(max_length=300, blank=True)
+    img = models.URLField(max_length=300, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    active = models.BooleanField()
+    active = models.BooleanField(blank = True)
