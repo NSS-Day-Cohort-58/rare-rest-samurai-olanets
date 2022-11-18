fb5198527051048a3321799602ec4d150a725bc1
Amanda De Leon <mandapanda6991@yahoo.com>
2022-11-18 15:07:47 -0600

    in progress

--- planetapi/models/__init__.py
+++ planetapi/models/__init__.py
@@ -1,9 +1,6 @@
 from .comment import Comment
 from .category import Category
 from .author import Author
-from .tag import Tag
 from .post import Post
-
-
-
-
+from .tag import Tag
+from .post_tag import PostTag
