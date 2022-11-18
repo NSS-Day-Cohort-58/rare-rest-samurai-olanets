799064fdf637928078eeb97cb42c71d9ecd5447d
Mckenzie Rondeau <mckenzierondeau@gmail.com>
2022-11-18 11:09:08 -0600

    trying to merge new main

--- planetapi/views/__init__.py
+++ planetapi/views/__init__.py
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 
 from .comment_view import CommentView
-
 from .category_view import CategoryView
-
-
 from .author_view import AuthorView
-
-=======
 from .posts_view import PostView
-from .category_view import CategoryView
-from .author_view import AuthorView
-from .auth import login_user, register_user
 
->>>>>>> main
+
+
