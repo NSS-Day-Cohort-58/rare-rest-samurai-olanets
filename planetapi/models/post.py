1eec0326336561abacd25c0f94a836548de19b2a
Russ Pelton <iamkrashenbern@yahoo.com>
2022-11-18 15:06:36 -0600

    Post List properly renders. memo: push client also.

--- planetapi/models/post.py
+++ planetapi/models/post.py
@@ -6,4 +6,5 @@ class Post(models.Model):
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
     date = models.DateField()
     image = models.CharField(max_length=512)
+    author = models.ForeignKey("Author", on_delete=models.CASCADE)
     
--- planetapi/views/posts_view.py
+++ planetapi/views/posts_view.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseServerError
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from rest_framework.decorators import action
-from planetapi.models import Post, Category
+from planetapi.models import Post, Category, Author
 
 class PostView(ViewSet):
     def create(self, request):
@@ -12,12 +12,14 @@ class PostView(ViewSet):
         Returns
         JSON serialized post instance"""
         category = Category.objects.get(pk=request.data["category"])
+        author = Author.objects.get(pk=request.data["author"])
         post = Post.objects.create(
             title = request.data["title"],
             content = request.data["content"],
             date= request.data["date"],
             image = request.data["image"],
-            category = category
+            category = category,
+            author = author
         )
         serializer = PostSerializer(post)
         return Response(serializer.data)
@@ -42,6 +44,7 @@ class PostView(ViewSet):
         post.date=request.data["date"]
         post.image=request.data["image"]
         post.category=request.data["category"]
+        post.author=request.data["author"]
         post.save()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
     
@@ -54,4 +57,5 @@ class PostSerializer(serializers.ModelSerializer):
     """JSON serializer for posts."""
     class Meta:
         model=Post
-        fields=("id", "title", "content", "category", "date", "image",)
+        fields=("id", "title", "content", "category", "date", "image", "author")
+        depth = 2
