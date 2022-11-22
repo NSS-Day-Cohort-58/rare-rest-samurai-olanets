ae91cdac493107d8dce4833bda53ae712eea0b79
Mckenzie Rondeau <mckenzierondeau@gmail.com>
2022-11-22 10:25:01 -0600

    added name property to author and serializer on comment_view

--- planetapi/models/comment.py
+++ planetapi/models/comment.py
@@ -4,3 +4,7 @@ class Comment(models.Model):
     author = models.ForeignKey("Author", on_delete=models.CASCADE, related_name="comment")
     post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comment")
     content = models.CharField(max_length=1000)
+
+    @property
+    def author_name(self):
+        return f'{self.author.full_name}'
